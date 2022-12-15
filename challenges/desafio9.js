c32ac156b3ee126d50f6e9b6fe8c78e458389ee5
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-12-15 20:40:24 -0300

    req08+09:feito

---
+++ challenges/desafio9.js
@@ -0,0 +1,14 @@
+db.produtos.find(
+  { 
+    valoresNutricionais: { 
+      $elemMatch: {
+        tipo: "calorias",
+        quantidade: { $lt: 500 },
+      },
+    },
+  },
+  {
+    nome: 1,
+    _id: 0,
+  },
+);
